1eecb5d00228d8f86b528db74961af618ab79d43
刘汤圆 <review617@163.com>
2020-03-11 15:58:23 +0800

    leetcode 66. 加一

--- AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
+++ AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
@@ -13,12 +13,14 @@ class EasyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print(plusOne([9,9,9]))
         
+//        print(plusOne([7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6]))
     }
     
      // MARK: 两数之和 2020.03.07
     /*
-     给定一个整数数组nums 和一个目标值target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     给定一个整数数组nums 和一个目标值target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 
          示例:
          给定 nums = [2, 7, 11, 15], target = 9
@@ -61,7 +63,57 @@ class EasyViewController: UIViewController {
          let str1 = String(str.reversed())
          return str == str1
      }
-    
+    // MARK: 加一 （如果直接把string转int，会有溢出，超过32位之后就无法成功转化）
+     /*
+     *给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+     示例 1:
+     输入: [1,2,3]
+     输出: [1,2,4]
+     解释: 输入数组表示数字 123。
+     
+     示例 2:
+     输入: [4,3,2,1]
+     输出: [4,3,2,2]
+     解释: 输入数组表示数字 4321。
+     */
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var digitArr = digits
+        if digits.count < 1 { return digits }
+        // 1. 如果末位加上1小于10，则直接返回
+        let last = digits.last!
+        if (last + 1) < 10 {
+            digitArr[digitArr.count-1] = last + 1
+            return digitArr
+        } else {
+            // 2. 如果末位加上1大于9，则轮询位数
+            let i = digitArr.count-1
+            digitArr[i] = 0
+            if digitArr.count == 1 {
+                digitArr.insert(1, at: 0)
+                return digitArr
+            } else {
+                digitArr[i-1] = digitArr[i-1] + 1
+                var index = i - 1
+                while index > -1 {
+                    if digitArr[index] > 9 {
+                        digitArr[index] = 0
+                        if index == 0 {
+                            digitArr.insert(1, at: 0)
+                        }else {
+                            digitArr[index-1] = digitArr[index-1] + 1
+                        }
+                     } else {
+                        return digitArr
+                    }
+                     index-=1
+                }
+                return digitArr
+             }
+         }
+     }
 }
 
 
