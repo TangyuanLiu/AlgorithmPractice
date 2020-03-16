3a5fa578f49f2321a655ad00738d3d7520546809
刘汤圆 <review617@163.com>
2020-03-16 23:03:36 +0800

    LeetCode 26. 删除排序数组中的重复项

--- AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
+++ AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
@@ -13,13 +13,59 @@ class EasyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         /* 旋转数组
         var arr = [1,2,3,4,5,6,7]
         rotate(&arr, 3)
         */
-        var nums1 = [4,0,0,0,0,0]
-        var nums2 = [1,2,3,5,6]
-        merge(&nums1, 1, nums2, 5)
+        
+        var nums = [1,2,3,3,6,8]
+        print(removeDuplicates(&nums))
+    }
+    // MARK: 删除排序数组中的重复项 2020.03.16
+    /*
+     给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+     示例 1:
+     给定数组 nums = [1,1,2],
+     函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+     你不需要考虑数组中超出新长度后面的元素。
+     
+     示例 2:
+     给定 nums = [0,0,1,1,1,2,2,3,3,4],
+     函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+     你不需要考虑数组中超出新长度后面的元素。
+      
+     说明:
+     为什么返回数值是整数，但输出的答案是数组呢?
+     请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+
+     你可以想象内部操作如下:
+     int len = removeDuplicates(nums);// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+
+     // 在函数里修改输入数组对于调用者是可见的。
+     // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+     for (int i = 0; i < len; i++) {
+         print(nums[i]);
+     }
+     
+     ps: 若数组中没有重复元素，按照上面的方法，每次比较时 nums[p] 都不等于 nums[q]，因此就会将 q 指向的元素原地复制一遍，这个操作其实是不必要的。
+     因此我们可以添加一个小判断，当 q - p > 1 时，才进行赋值。
+     */
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        if nums.count < 2 { return nums.count }
+        var i = 0, j = 1
+        while j < nums.count {
+            if nums[i] == nums[j] {
+                j+=1
+            } else {
+                nums[i+1] = nums[j] //当 q - p > 1 时，才进行赋值
+                i+=1
+                j+=1
+            }
+        }
+        return i + 1
     }
     // MARK: 合并两个有序数组 2020.03.13
     /*
