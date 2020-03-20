2165c46e5f29e7db85e2d0225bc930ca8727031e
刘汤圆 <review617@163.com>
2020-03-20 22:59:58 +0800

    LeetCode 49. 字母异位词分组

--- AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
+++ AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
@@ -18,9 +18,35 @@ class EasyViewController: UIViewController {
         var arr = [1,2,3,4,5,6,7]
         rotate(&arr, 3)
         */
-        print(getLeastNumbers([0,0,1,2,4,2,2,3,1,4], 8))
+        print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    }
+    // MARK: 字母异位词分组 2020.03.20
+    /*
+     给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+     示例:
+     输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+     输出:
+     [
+       ["ate","eat","tea"],
+       ["nat","tan"],
+       ["bat"]
+     ]
+     说明：
+     所有输入均为小写字母。
+     不考虑答案输出的顺序。
+     */
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        if strs.count == 0 { return [] }
+        var result = [String: [String]]()
+        for str in strs {
+            let sort_str = String(Array(str).sorted())
+            if !result.keys.contains(sort_str) {
+                result[sort_str] = [String]()
+            }
+            result[sort_str]?.append(str)
+        }
+        return Array(result.values)
         
-        print(findKthLargest([3,2,1,5,6,4], 2))
     }
     // MARK: 数组中的第K个最大元素 2020.03.20
     /*
