3ac72c4076125610ed7b23b75acae17167c5f909
刘汤圆 <review617@163.com>
2020-03-20 14:14:08 +0800

    LeetCode 215. 数组中的第K个最大元素

--- AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
+++ AlgorithmPractice/AlgorithmPractice/EasyViewController.swift
@@ -19,9 +19,31 @@ class EasyViewController: UIViewController {
         rotate(&arr, 3)
         */
         print(getLeastNumbers([0,0,1,2,4,2,2,3,1,4], 8))
-
+        
+        print(findKthLargest([3,2,1,5,6,4], 2))
     }
-    // MARK: 面试题40. 最小的k个数
+    // MARK: 数组中的第K个最大元素 2020.03.20
+    /*
+     在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+     示例 1:
+     输入: [3,2,1,5,6,4] 和 k = 2
+     输出: 5
+     
+     示例 2:
+     输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+     输出: 4
+     
+     说明:
+     你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+     */
+    func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+        let new_nums = nums.sorted { (i, j) -> Bool in
+            i > j
+        }
+        print(new_nums)
+        return new_nums[k-1]
+    }
+    // MARK: 面试题40. 最小的k个数 2020.03.20
     /*
      输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
 
